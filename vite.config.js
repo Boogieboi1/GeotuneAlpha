f8cfb8c87290bf81a887b9a81182544d267898c0
Boogieboi <baz_graham@live.nl>
2026-03-21 10:15:30 +0100

    vjemoeder

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,19 @@ import restart from 'vite-plugin-restart'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/GeotuneAlpha/' : '',
-    root: 'src/',
-    publicDir: '../static/',
-    server:
-    {
-        host: true,
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    },
-    plugins:
-    [
-        restart({ restart: ['../static/**'] })
-    ]
+  base: process.env.NODE_ENV === 'production' ? '/GeotuneAlpha/' : '/',
+  root: 'src/',
+  publicDir: '../static/',
+  server: {
+    host: true,
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  plugins: [
+    restart({ restart: ['../static/**'] })
+  ]
 })
