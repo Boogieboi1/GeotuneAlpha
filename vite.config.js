733fee7d4d2370bed0035fa705f559a470dc6649
Boogieboi <baz_graham@live.nl>
2026-03-21 09:46:03 +0100

    v1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import restart from 'vite-plugin-restart'
 
 export default {
+    
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
@@ -18,4 +19,5 @@ export default {
     [
         restart({ restart: [ '../static/**', ] }) // Restart server on static file change
     ],
+    base: process.env.NODE_ENV === 'production' ? '/    GeotuneAlpha/' : '',
 }
